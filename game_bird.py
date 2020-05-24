9e4286f9fd1df44a9e35c4316e796f0b42a098ed
Kristina-212 <kristinagladysheva05@mail.ru>
2020-05-24 18:46:33 +0700

    Update game_bird.py
    added background
    added bird's model
    added functions of walking and jumping

--- game_bird.py
+++ game_bird.py
@@ -5,35 +5,121 @@ win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption('Bird')  # заголовок окна
 
+flyLeft = [pygame.image.load('bird_left_fly_1.png'), pygame.image.load('bird_left_fly_2.png'),
+           pygame.image.load('bird_left_fly_3.png'), pygame.image.load('bird_left_fly_4.png')]
+
+flyRight = [pygame.image.load('bird_right_fly_1.png'), pygame.image.load('bird_right_fly_2.png'),
+            pygame.image.load('bird_right_fly_3.png'), pygame.image.load('bird_right_fly_4.png')]
+
+flyUp = [pygame.image.load('bird_up_fly_1.png'), pygame.image.load('bird_up_fly_2.png'),
+         pygame.image.load('bird_up_fly_3.png'), pygame.image.load('bird_up_fly_4.png')]
+
+flyDown = [pygame.image.load('bird_down_fly_1.png'), pygame.image.load('bird_down_fly_2.png'),
+           pygame.image.load('bird_down_fly_3.png'), pygame.image.load('bird_down_fly_4.png')]
+
+birdStand = pygame.image.load('bird_front_1.png')
+
+walkBack = [pygame.image.load('bird_back_1.png'), pygame.image.load('bird_back_2.png'),
+            pygame.image.load('bird_back_3.png')]
+
+walkLeft = [pygame.image.load('bird_left_1.png'), pygame.image.load('bird_left_2.png'),
+            pygame.image.load('bird_left_3.png')]
+
+walkRight = [pygame.image.load('bird_right_1.png'), pygame.image.load('bird_right_2.png'),
+             pygame.image.load('bird_right_3.png')]
+
+bg = pygame.image.load('forest(500x500).jpg')
+
+clock = pygame.time.Clock()
+
 x = 50
-y = 430
-wight = 40
-height = 60
+y = 400
+wight = 30
+height = 30
 speed = 5
+
+isJump = False
+jumpCount = 10
+
+left = False
+right = False
+up = False
+down = False
+animCount = 0
+
+
+def drawWindow():
+    global animCount
+    win.blit(bg, (0, 0))
+
+    if animCount + 1 >= 30:  # 30 fps
+        animCount = 0
+
+    if left:
+        win.blit(walkLeft[animCount // 30], (x, y))
+        animCount += 1
+    elif right:
+        win.blit(walkRight[animCount // 30], (x, y))
+        animCount += 1
+    elif up:
+        win.blit(walkBack[animCount // 30], (x, y))
+        animCount += 1
+    else:
+        win.blit(birdStand, (x, y))
+
+    pygame.display.update()
+
 
 run = True
 while run:
-    pygame.time.delay(100)  # кол-во милисекунд, через которое будет обратно выполняться цикл
+    clock.tick(30)
 
-    # отслеживание событий  +  отслеживание единоразового нажатия на клавишу
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    # отслеживание событий при зажатии клавиши
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and x > 5:
         x -= speed
-    if keys[pygame.K_RIGHT] and x < 500 - wight - 5:
+        left = True
+        right = False
+        up = False
+        down = False
+    elif keys[pygame.K_RIGHT] and x < 500 - wight - 5:
         x += speed
-    if keys[pygame.K_UP] and y > 5:
+        left = False
+        right = True
+        up = False
+        down = False
+    elif keys[pygame.K_UP] and y > 370:
         y -= speed
-    if keys[pygame.K_DOWN] and y < 500 - height - 5:
+        left = False
+        right = False
+        up = True
+        down = False
+    elif keys[pygame.K_DOWN] and y < 500 - height - 5:
         y += speed
+        left = False
+        right = False
+        up = False
+        down = True
+    else:
+        animCount = 0
 
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (0, 0, 225), (x, y, wight, height))  # при запуске ничего не будет отображено на экране
-    # окно нужно постоянно обновлять
-    pygame.display.update()
+    if not isJump:
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            if jumpCount < 0:
+                y += (jumpCount ** 2) / 4
+            else:
+                y -= (jumpCount ** 2) / 4
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+
+    drawWindow()
 
 pygame.quit()
